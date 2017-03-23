4288aaaefa59ef90fe6710946beb54f15704191e
Keith Oppel <mistakeets@gmail.com>
2017-03-23 11:36:34 -0700

    finishes exercise 12 with all tests passing

---
+++ promise-it-wont-hurt-solutions/src/12-fetch-json.js
@@ -0,0 +1,8 @@
+const qHttp = require('q-io/http')
+
+qHttp.read('http://localhost:1337')
+  .then(function(data) {
+    console.log(JSON.parse(data));
+  })
+  .then(null, console.error)
+  .done()
